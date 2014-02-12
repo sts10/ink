fdcaf9f25c1add6e1a811efaa8c5d95890192648
sts10 <sschlinkert@gmail.com>
2014-02-12 02:37:10 -0500

    initial ink commit.

---
+++ README.md
@@ -0,0 +1,48 @@
+ink
+========
+
+A shell script that serves as a crude CMS for Octopress blogs. 
+
+v 0.3
+
+
+### Installation (as of v 0.3)
+
+**Recommend you read through the code first** 
+
+To install ink v 0.3, 
+
+1. clone my Git repo. I’d recommend cloning it somewhere in your “code” directory. Inside you’ll find a .sh file and a README. 
+2. Open the ink.sh file in a text editor like Sublime. On line 9, put in the path to your Octopress directory (user.github.io). Then verify that line 17 is the correct path from your github.io directory to your posts. 
+3. Now navigate to your user.github.io directory in your terminal. if you run ```git branch``` you should see two branches: “source” and “master”. Create a new branch called “drafts” but typing ```git branch drafts```. Do NOT checkout the new “drafts” branch, just create. Keep “source” checked out.  
+4. Next, you’ll want to put a function in your bash profile so you can call ink from anywhere in your terminal. To open your bash profile, run the following in your command line: ```open ~/.bash_profile```. Paste in this function in the top level:
+```
+function ink { bash /Users/$USER/Documents/code/ink/ink.sh "$1" }
+```
+Close and reopen your terminal. You should now be able to call ```ink``` from anywhere in your terminal to run ink.
+
+
+### Use
+
+From anywhere in the terminal, user can call ```$ ink “blog post title”```.
+
+Then user’s default editor launches. The user writes his or her post, saves it, closes the editor. 
+
+User should return to the open terminal window and user will find a menu with three choices: publish, delete, and quit. 
+
+**Publish** adds, commits, and pushes the user’s Git up to GitHub, then deploys your Octopress blog. Here’s the Bash code:
+
+``` bash
+  git add .
+  git commit -m  "Used ink to publish a new post called "$FILENAME"." 
+  git push origin source
+  rake generate
+  rake deploy 
+```
+and then takes you back to the directory from which you called ```ink```.
+
+**Save** moves the file to the users’ “drafts” branch and stores it there. This way publishing the blog through the rake commands does NOT publish the drafts. Users can access their save drafts by entering ```ink “drafts”``` from anywhere in the terminal. 
+
+**Delete** removes the file of the post you just created. It also removes it from the source branch of your local Git repo commits it, and pushes to your remote source branch with ```git push origin source```. It then returns the user to the directory from which they originally called ```ink```.
+
+**Quit** just returns user to the directory from which they originally called ```ink```. It leaves the Git not added, not pushed, and not pushed. It also does not run ```rake generate``` or ```rake deploy```.
---
+++ ink.sh
@@ -0,0 +1,186 @@
+# ink
+
+  INPUT="$1"
+  
+
+  cd /Users/$USER/Documents/code/sts10.github.io 
+  
+
+
+  if [[ $INPUT == "drafts" ]]
+  then
+    git checkout drafts
+
+     cd source/_posts/
+
+
+    FILENAME="q"
+
+    PS3="Type a number or 'q' to quit: "
+  
+    # Create a list of files to display  http://wuhrr.wordpress.com/2009/09/10/simple-menu-with-bashs-select-command/
+    fileList=$(find . -maxdepth 1 -type f \( ! -iname ".*" \))  # ignores dot files like .DS_STORE 
+
+    
+
+    # Show a menu and ask for input. 
+    select draftFileName in $fileList; do
+        if [ -n "$draftFileName" ]; then
+            FILENAME=${draftFileName}
+        fi
+        break
+    done
+
+
+  
+
+    if [[ $FILENAME == "q" ]]
+    then
+      echo ''
+      echo "Sorry, I don't have that draft. Goodbye."
+      git checkout source 
+      exit 
+    fi
+
+
+    if [ ! -f $FILENAME ]  # if user entry does not match an existing draft file name. 
+    then
+      echo ''
+      echo "Sorry, I don't have that draft. Goodbye."
+      git checkout source 
+      # cd $cwd
+      exit 
+    fi
+
+    # move $FILENAME from drafts to source, assuming we'll publish it
+    git checkout source
+    git checkout drafts -- $FILENAME  # get draft from drafts branch, put in source branch
+
+      # commit changes to source branch 
+      git add . 
+      git commit -m "move your selected draft to source branch"
+
+
+    # (temporarily) delete $FILENAME from the drafts branch
+    git checkout drafts
+      rm $FILENAME
+      git add --all . 
+      git commit -m "move your selected draft to source branch"
+
+    git checkout source
+
+    open $FILENAME
+
+    
+
+  else
+    rake new_post["$INPUT"]
+    echo "Creating new octopress post called \""$INPUT"\""
+
+    cd source/_posts
+    FILENAME=`ls -t | head -1`
+    open $FILENAME
+  fi
+  
+  
+ 
+  clear 
+  echo "Welcome to ink!"
+  echo ""
+  echo "I just opened a file called "$FILENAME" for you! Go write an awesome post!"
+
+  echo ''
+  echo "Once you've saved the file of your new post, here are your options:"
+  echo ''
+  echo "p - publish your octopress blog and commit and push your source branch to GitHub"
+  echo 's - save this post as a draft. Drafts are accessible by entering ink "drafts" on the command line.'
+  echo "d - delete the post you just wrote, and remove it from the source branch of your local Git repo"
+  echo "q - quit without doing either of the above"
+  echo ''
+
+
+  read -p "" -n 1 -r  
+  echo ''  
+
+  git checkout source
+
+  if [[ $REPLY =~ ^[Pp]$ ]]    # if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+      # commit git and publish blog
+      
+      cd ../../
+      
+      
+      git add .
+      git commit -m  "Used ink to publish a new post called "$FILENAME"." # $FILENAME ?
+
+      git push origin source
+      rake generate
+      rake deploy 
+
+    # cd $cwd
+
+  elif [[ $REPLY =~ ^[Dd]$ ]]
+  then 
+      echo ''
+      echo "Are you sure you want to delete "$FILENAME"? (y/n) "
+      read REPLY2 
+      if [[ $REPLY2 =~ ^[Yy]$ ]]
+      then
+        rm $FILENAME
+        git add --all .
+        git commit -m "Deleted post "$FILENAME" using ink."
+        git push origin source
+        echo ''
+        echo "Deleted "$FILENAME", removed it from Git, and committed and pushed Git"  
+        # cd $cwd
+      else
+        echo "OK, we'll just leave it in your _posts folder."
+        cd ../../
+      fi
+  elif [[ $REPLY =~ ^[Ss]$ ]]
+  then 
+
+    cd ../../
+
+    git add .
+    git commit -m "add draft to source branch temporarily"
+
+    git checkout drafts
+
+    if [ ! -f source/_posts/$FILENAME ]  #if this IS a new draft
+    then
+      echo "Saving new draft!"
+
+      touch source/_posts/$FILENAME
+      
+    fi 
+
+    git add .
+      git commit -m "add blank"
+
+
+    git checkout source -- source/_posts/$FILENAME  # move the file I'm working on to drafts
+
+    git add .
+    git commit -m "add draft "$FILENAME" to drafts branch."
+
+
+    # clean up the source branch
+
+    git checkout source
+
+    cd source/_posts
+
+    rm $FILENAME
+        git add --all .
+        git commit -m "Moved post "$FILENAME" to drafts branch and remove from source branch."
+        git push origin source
+
+    cd ../../
+
+  else
+    echo "OK, we'll just leave this post in your source/_posts directory, unstaged."
+    cd ../../  # return to main octopress directory  
+
+  fi
